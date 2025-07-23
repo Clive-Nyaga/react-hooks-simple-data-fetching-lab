dfb8459d72697821110417f0846962ed8077967f
Clive Nyaga Kagotho <clive.kagotho@student.moringaschool.com>
2025-07-23 16:11:33 +0300

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => setData(data.message))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div>
+        {data ? <img src={data} alt=" A Random Dog" /> : 'Loading...'}
+    </div>
+  );
+}
+
+export default App;
